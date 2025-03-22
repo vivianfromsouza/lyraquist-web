452f1c0ded434d31354a41ec4d7242f0b51b3dc1
Vivian D'Souza <vivian.dsouza.789@gmail.com>
2025-03-22 18:40:35 -0400

    web player now loads last played song from db

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -20,23 +20,27 @@ const Player = () => {
   const accessToken = UserReaderWriter.getUserAccessCode();
 
   const location = useLocation();
-  const [playItem, setPlayItem] = useState(location.state || "0lUSd7TCG8srh6HpIAEIWL");
-
+  // const playItem = {
+  //   name: "Everyway That I Can - Aytekin Kurt, Murat Uncuoğlu Remix",
+  //   spotifyURL: "https://i.scdn.co/image/ab67616d0000b27387d19f64a67b70c87510dcca",
+  //   imageURL: "https://i.scdn.co/image/ab67616d0000b27387d19f64a67b70c87510dcca",
+  //   artist: "Sertab",
+  // };
   // const playItem = location.state;
-  const [currentURL] = useState(playItem.spotifyURL);
+  const [currentURL] = useState("");
 
   const track = {
-    name: playItem.name,
-    spotifyURL: playItem.spotifyURL,
+    name: "trackName",
+    spotifyURL: "trackURL",
     album: {
-      images: [{ url: playItem.imageURL }],
+      name: "trackAlbum",
+      images: [{ url: "trackImage" }],
     },
-    artists: [{ name: playItem.artist }],
+    artists: [{ name: "trackArtist" }],
+    duration: "trackDuration",
   };
 
-  const [current_track, setTrack] = useState(track);
-
-  console.log(current_track);
+  const [current_track, setCurrentTrack] = useState(track);
 
   async function playAnySong() {
     UserReaderWriter.getUserAccessCode().then((accessCode) => {
@@ -60,9 +64,28 @@ const Player = () => {
     });
   }
 
+  async function getCurrentTrack() {
+    await UserReaderWriter.getCurrentTrackDetails().then((track) => {
+      if (track != null) {
+        setCurrentTrack({
+          name: track["songs"]["name"],
+          spotifyURL: track["songs"]["spotify_url"],
+          album: {
+            name: track["songs"]["album"],
+            images: [{ url: track["songs"]["image_url"] }],
+          },
+          artists: [{ name: track["songs"]["artist"] }],
+          duration: track["songs"]["duration"],
+        });
+      }
+      console.log("CURR TRACK:");
+      console.log(track);
+    });
+  }
+
   // this is running x2....
   useEffect(() => {
-    // getSpotifyAccessCode()
+    getCurrentTrack();
     const script = document.createElement("script");
     script.src = "https://sdk.scdn.co/spotify-player.js";
     script.async = true;
@@ -92,12 +115,12 @@ const Player = () => {
 
       player.addListener("player_state_changed", (state) => {
         console.log("song changed");
-        setTrack(track);
+        // setTrack(track);
         if (!state) {
           return;
         }
 
-        setTrack(state.track_window.current_track);
+        setCurrentTrack(state.track_window.current_track);
         setPaused(state.paused);
 
         player.getCurrentState().then((state) => {
@@ -111,77 +134,77 @@ const Player = () => {
     };
   }, []);
 
-  if (!is_active) {
-    return (
-      <>
-        <div className="container">
-          <div className="main-wrapper">
-            <b>
-              {" "}
-              Instance not active. Transfer your playback using your Spotify app{" "}
-            </b>
-          </div>
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div className="container">
-          <div className="main-wrapper">
-            <img
-              src={current_track.album.images[0].url}
-              className="now-playing__cover"
-              alt=""
-            />
+  // if (!is_active) {
+  // return (
+  //   <>
+  //     <div className="container">
+  //       <div className="main-wrapper">
+  //         <b>
+  //           {" "}
+  //           Instance not active. Transfer your playback using your Spotify app{" "}
+  //         </b>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
+  // } else {
+  return (
+    <>
+      <div className="container">
+        <div className="main-wrapper">
+          <img
+            src={current_track.album.images[0].url}
+            className="now-playing__cover"
+            alt=""
+          />
 
-            <div className="now-playing__side">
-              <div className="now-playing__name">{current_track.name}</div>
-              <div className="now-playing__artist">
-                {current_track.artists[0].name}
-              </div>
-
-              <button
-                className="btn-spotify"
-                onClick={() => {
-                  player.previousTrack();
-                }}
-              >
-                &lt;&lt;
-              </button>
-
-              <button
-                className="btn-spotify"
-                onClick={() => {
-                  player.togglePlay();
-                }}
-              >
-                {is_paused ? "PLAY" : "PAUSE"}
-              </button>
-
-              <button
-                className="btn-spotify"
-                onClick={() => {
-                  player.nextTrack();
-                }}
-              >
-                &gt;&gt;
-              </button>
-
-              <button
-                className="btn-spotify"
-                onClick={() => {
-                  playAnySong();
-                }}
-              >
-                PLAY ANY SONG
-              </button>
+          <div className="now-playing__side">
+            <div className="now-playing__name">{current_track.name}</div>
+            <div className="now-playing__artist">
+              {current_track.artists[0].name}
             </div>
+
+            <button
+              className="btn-spotify"
+              onClick={() => {
+                player.previousTrack();
+              }}
+            >
+              &lt;&lt;
+            </button>
+
+            <button
+              className="btn-spotify"
+              onClick={() => {
+                player.togglePlay();
+              }}
+            >
+              {is_paused ? "PLAY" : "PAUSE"}
+            </button>
+
+            <button
+              className="btn-spotify"
+              onClick={() => {
+                player.nextTrack();
+              }}
+            >
+              &gt;&gt;
+            </button>
+
+            <button
+              className="btn-spotify"
+              onClick={() => {
+                playAnySong();
+              }}
+            >
+              PLAY ANY SONG
+            </button>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+  // }
 };
 
 export default Player;
