2118e02d5c6498b63094a5d7a2b4974ae4bc1043
Vivian D'Souza <vivian.dsouza.789@gmail.com>
2025-03-26 19:42:03 -0400

    playback ends upon signing out

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -17,7 +17,9 @@ const Player = () => {
   // );
   const [player, setPlayer] = useState(null);
   const [device_id, setDeviceId] = useState("abc");
+
   const accessToken = UserReaderWriter.getUserAccessCode();
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
 
   const location = useLocation();
   // const playItem = {
@@ -65,7 +67,7 @@ const Player = () => {
   }
 
   async function transferPlayback(device_id: string) {
-    console.log("TRANSFERRING PLAYBACK")
+    console.log("TRANSFERRING PLAYBACK");
     UserReaderWriter.getUserAccessCode().then((accessCode) => {
       // Makes request to Spotify API for song search
       axios({
@@ -76,7 +78,7 @@ const Player = () => {
         },
         data: {
           device_ids: [device_id],
-          play: false // keeps it off if it's paused
+          play: false, // keeps it off if it's paused
         },
       })
         .then(async (res) => {
@@ -86,154 +88,161 @@ const Player = () => {
           return err;
         });
     });
-    
   }
 
-  async function getCurrentTrack() {
-    await UserReaderWriter.getCurrentTrackDetails().then((track) => {
-      if (track != null) {
-        setCurrentTrack({
-          name: track["songs"]["name"],
-          spotifyURL: track["songs"]["spotify_url"],
-          album: {
-            name: track["songs"]["album"],
-            images: [{ url: track["songs"]["image_url"] }],
-          },
-          artists: [{ name: track["songs"]["artist"] }],
-          duration: track["songs"]["duration"],
+  async function pausePlayback() {
+    console.log("ENDING PLAYBACK");
+    UserReaderWriter.getUserAccessCode().then((accessCode) => {
+      // Makes request to Spotify API for song search
+      axios({
+        url: "https://api.spotify.com/v1/me/player/pause", // Remove "&limit=1"
+        method: "PUT",
+        headers: {
+          authorization: "Bearer " + accessCode,
+        },
+        data: {
+          device_ids: [device_id],
+        },
+      })
+        .then(async (res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          return err;
         });
-      }
-      console.log("CURR TRACK:");
-      console.log(track);
     });
   }
 
-  // this is running x2....
-  useEffect(() => {
-    // getCurrentTrack();
-    const script = document.createElement("script");
-    script.src = "https://sdk.scdn.co/spotify-player.js";
-    script.async = true;
+    // this is running x2....
+    useEffect(() => {
+      if (isLoggedIn) {
+        const script = document.createElement("script");
+        script.src = "https://sdk.scdn.co/spotify-player.js";
+        script.async = true;
 
-    document.body.appendChild(script);
+        document.body.appendChild(script);
 
-    window.onSpotifyWebPlaybackSDKReady = () => {
-      console.log(accessToken);
-      const player = new window.Spotify.Player({
-        name: "Web Playback SDK",
-        getOAuthToken: (cb) => {
-          cb(accessToken);
-        },
-        volume: 0.5,
-      });
+        window.onSpotifyWebPlaybackSDKReady = () => {
+          console.log(accessToken);
+          const player = new window.Spotify.Player({
+            name: "Web Playback SDK",
+            getOAuthToken: (cb) => {
+              cb(accessToken);
+            },
+            volume: 0.5,
+          });
 
-      setPlayer(player);
+          setPlayer(player);
 
-      player.addListener("ready", ({ device_id }) => {
-        console.log("Ready with Device ID", device_id);
-        setDeviceId(device_id);
-        transferPlayback(device_id);
-        player.getCurrentState().then((state) => {
-          !state ? setActive(false) : setActive(true);
-        });
-      });
+          player.addListener("ready", async ({ device_id }) => {
+            console.log("Ready with Device ID", device_id);
+            setDeviceId(device_id);
+            await transferPlayback(device_id);
+            player.getCurrentState().then((state) => {
+              !state ? setActive(false) : setActive(true);
+            });
+          });
 
-      player.addListener("not_ready", ({ device_id }) => {
-        console.log("Device ID has gone offline", device_id);
-      });
+          player.addListener("not_ready", ({ device_id }) => {
+            console.log("Device ID has gone offline", device_id);
+          });
 
-      player.addListener("player_state_changed", (state) => {
-        console.log("song changed");
-        // setTrack(track);
-        if (!state) {
-          return;
-        }
+          player.addListener("player_state_changed", (state) => {
+            console.log("song changed");
+            // setTrack(track);
+            if (!state) {
+              return;
+            }
 
-        setCurrentTrack(state.track_window.current_track);
-        setPaused(state.paused);
+            setCurrentTrack(state.track_window.current_track);
+            setPaused(state.paused);
 
-        player.getCurrentState().then((state) => {
-          !state ? setActive(false) : setActive(true);
-        });
-      });
+            player.getCurrentState().then((state) => {
+              !state ? setActive(false) : setActive(true);
+            });
+          });
 
-      console.log(accessToken);
+          console.log(accessToken);
 
-      player.connect();
-    };
-  }, []);
+          player.connect();
+        };
+      } else {
+        pausePlayback();
+      }
+    }, [isLoggedIn]);
 
-  if (!is_active) {
-  return (
-    <>
-      <div className="container">
-        <div className="main-wrapper">
-          <b>
-            {" "}
-            Instance not active. Transfer your playback using your Spotify app{" "}
-          </b>
-        </div>
-      </div>
-    </>
-  );
-  } else {
-  return (
-    <>
-      <div className="container">
-        <div className="main-wrapper">
-          <img
-            src={current_track.album.images[0].url}
-            className="now-playing__cover"
-            alt=""
-          />
-
-          <div className="now-playing__side">
-            <div className="now-playing__name">{current_track.name}</div>
-            <div className="now-playing__artist">
-              {current_track.artists[0].name}
+    if (!is_active) {
+      return (
+        <>
+          <div className="container">
+            <div className="main-wrapper">
+              <b>
+                {" "}
+                Instance not active. Transfer your playback using your Spotify
+                app{" "}
+              </b>
             </div>
-
-            <button
-              className="btn-spotify"
-              onClick={() => {
-                player.previousTrack();
-              }}
-            >
-              &lt;&lt;
-            </button>
-
-            <button
-              className="btn-spotify"
-              onClick={() => {
-                player.togglePlay();
-              }}
-            >
-              {is_paused ? "PLAY" : "PAUSE"}
-            </button>
-
-            <button
-              className="btn-spotify"
-              onClick={() => {
-                player.nextTrack();
-              }}
-            >
-              &gt;&gt;
-            </button>
-
-            <button
-              className="btn-spotify"
-              onClick={() => {
-                playAnySong();
-              }}
-            >
-              PLAY ANY SONG
-            </button>
           </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="container">
+            <div className="main-wrapper">
+              <img
+                src={current_track.album.images[0].url}
+                className="now-playing__cover"
+                alt=""
+              />
+
+              <div className="now-playing__side">
+                <div className="now-playing__name">{current_track.name}</div>
+                <div className="now-playing__artist">
+                  {current_track.artists[0].name}
+                </div>
+
+                <button
+                  className="btn-spotify"
+                  onClick={() => {
+                    player.previousTrack();
+                  }}
+                >
+                  &lt;&lt;
+                </button>
+
+                <button
+                  className="btn-spotify"
+                  onClick={() => {
+                    player.togglePlay();
+                  }}
+                >
+                  {is_paused ? "PLAY" : "PAUSE"}
+                </button>
+
+                <button
+                  className="btn-spotify"
+                  onClick={() => {
+                    player.nextTrack();
+                  }}
+                >
+                  &gt;&gt;
+                </button>
+
+                <button
+                  className="btn-spotify"
+                  onClick={() => {
+                    playAnySong();
+                  }}
+                >
+                  PLAY ANY SONG
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    }
   }
-};
 
 export default Player;
