f267b5717eb5cab9d2769b58b3696485d441479b
Vivian D'Souza <vivian.dsouza.789@gmail.com>
2025-05-03 20:08:25 -0400

    first tests

---
+++ src/test/Home.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it, expect, afterEach, beforeEach } from "vitest";
+import { page } from '@vitest/browser/context'
+import { render } from "@testing-library/react";
+import { Home } from "react-ionicons";
+
+describe("Home", () => {
+  it("renders Home component", () => {
+    render(<Home />);
+    expect(page.getByText('Hello')).toBeInTheDocument();
+  });
+});
