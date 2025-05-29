33637d6016d111c02fb8ec3a7727ba6dbe9abe60
alybickham <bickhaml@email.sc.edu>
2025-05-29 14:20:29 -0400

    fixed imports for services, todo fix screens

--- src/services/DefinitionService.ts
+++ src/services/DefinitionService.ts
@@ -1,83 +1,99 @@
-// // Worked on by: Ashley Bickham
-// // TODO: USE NEW CONFIG HERE
+// Worked on by: Ashley Bickham
+// TODO: USE NEW CONFIG HERE
 
-// import axios from "axios";
-// import { APIKeys } from "../APIKeys";
+import axios from "axios";
 
-// //var to hold partOfSpeech
-// var partOfSpeech = '';
+//var to hold partOfSpeech
+var partOfSpeech = '';
 
-// //This service handles queries and responses to Lexicala API which will pull word definitions and tentativey words' part of speech (POS)
-// const DefinitionService = {
+//This service handles queries and responses to Lexicala API which will pull word definitions and tentativey words' part of speech (POS)
+const DefinitionService = {
 
-//   /*grabs word Defintiion and also the POS from same API call
-//   -did here to minimize API calls as the free version of Lexicala allows only 50 calls per day)*/
-//   async getDefinition(word, wordLang) : Promise<string> {
-//     if (word === undefined || word == "" || wordLang === undefined || wordLang == "" ){return ""}
+  /*grabs word Defintiion and also the POS from same API call
+  -did here to minimize API calls as the free version of Lexicala allows only 50 calls per day)*/
+  async getDefinition(word, wordLang) : Promise<string> {
+    if (word === undefined || word == "" || wordLang === undefined || wordLang == "" ){return ""}
     
-//     // to hold definition
-//     var def = '';
-//     //resets POS value for each call
-//     partOfSpeech = '';
+    // to hold definition
+    var def = '';
+    //resets POS value for each call
+    partOfSpeech = '';
 
-//     // settings for API call
-//     const options = {
-//       method: 'GET',
-//       url: 'https://lexicala1.p.rapidapi.com/search-entries?source=global&language='+wordLang+'&text='+word+'&morph=true',
-//       headers: {
-//         'X-RapidAPI-Key': APIKeys.DefKey,
-//         'X-RapidAPI-Host': 'lexicala1.p.rapidapi.com'
-//       }
-//     };
+    // settings for API call
+    const options = {
+      method: 'GET',
+      url: 'https://lexicala1.p.rapidapi.com/search-entries?source=global&language='+wordLang+'&text='+word+'&morph=true',
+      headers: {
+        'X-RapidAPI-Key': process.env.LEXICALA_API_KEY,
+        'X-RapidAPI-Host': 'lexicala1.p.rapidapi.com'
+      }
+    };
 
-//     // Makes api call and parses out the definition returns if there was an error
-//     try {
-//         const response = await axios.request(options);
-//         // counts to make sure we only grab first definiton and POS
-//         var count = 0;
-//         var countPOS = 0 
+    // Makes api call and parses out the definition returns if there was an error
+    try {
+        const response = await axios.request(options);
+        // counts to make sure we only grab first definiton and POS
+        var count = 0;
+        var countPOS = 0 
       
-//         //api call response is a jsosn object you must iterate through to access Sense property which contains an array of definitions
-//         response.data.results.forEach(results => {
+        //api call response is a jsosn object you must iterate through to access Sense property which contains an array of definitions
+        response.data.results.forEach(results => {
 
-//           // inside senses array of results JSON
-//           results.senses.forEach(senses => {
-//             /*Because of ForEach needed to iterate through JSON response object array, multiple deifnitions may be pulled if there are multiple
-//             Only need the first one, which is the most common usage of the word*/
-//             if (count == 0) {
-//               def = senses.definition;;
-//               count++
-//             }
-//           })
+          // inside senses array of results JSON
+          results.senses.forEach(senses => {
+            /*Because of ForEach needed to iterate through JSON response object array, multiple deifnitions may be pulled if there are multiple
+            Only need the first one, which is the most common usage of the word*/
+            if (count == 0) {
+              def = senses.definition;;
+              count++
+            }
+          })
 
-//           //grabbing word's part of speech
-//             if (countPOS == 0) {
-//               partOfSpeech = results.headword.pos;
-//               countPOS++
-//             }
+          //grabbing word's part of speech
+            if (countPOS == 0) {
+              partOfSpeech = results.headword.pos;
+              countPOS++
+            }
 
-//         });
-//       } catch (error) {
-//         console.error("Error grabbing DEF: " + error);
-//         //If Error is 429 we hit the API limit
-//         if (error.response.status == 429) {
-//           def = "HIT LEXICALA'S API CALL LIMIT"
-//           partOfSpeech = "HIT LEXICALA'S API CALL LIMIT";
-//         }
-//         else {
-//           def = 'ERROR: ' + error.response.status; 
-//           partOfSpeech = 'ERROR: ' + error.response.status;
-//         }
-//       }
+        });
+      } catch (error) {
+        console.error("Error grabbing DEF:", error);
 
-//     return def
-//   },
+        if (axios.isAxiosError(error) && error.response) {
+          const status = error.response.status;
 
-//   // returns part of speech found and assigned from within getDefinition function
-//   getPartOfSpeech() : string {
-//     return partOfSpeech
-//   }
-// }
+          if (status === 429) {
+            def = "HIT LEXICALA'S API CALL LIMIT";
+            partOfSpeech = "HIT LEXICALA'S API CALL LIMIT";
+          } else {
+            def = `ERROR: ${status}`;
+            partOfSpeech = `ERROR: ${status}`;
+          }
+        } else {
+          def = "ERROR: Unknown error";
+          partOfSpeech = "ERROR: Unknown error";
+        }
+
+        // console.error("Error grabbing DEF: " + error);
+        // //If Error is 429 we hit the API limit
+        // if (error.response.status == 429) {
+        //   def = "HIT LEXICALA'S API CALL LIMIT"
+        //   partOfSpeech = "HIT LEXICALA'S API CALL LIMIT";
+        // }
+        // else {
+        //   def = 'ERROR: ' + error.response.status; 
+        //   partOfSpeech = 'ERROR: ' + error.response.status;
+        // }
+      }
+
+    return def
+  },
+
+  // returns part of speech found and assigned from within getDefinition function
+  getPartOfSpeech() : string {
+    return partOfSpeech
+  }
+}
 
   
-//   export default DefinitionService;
+  export default DefinitionService;
--- src/services/TranslationService.ts
+++ src/services/TranslationService.ts
@@ -1,125 +1,124 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// // Worked on by: Ashley Bickham and Siri Avula
-// // TODO: FIX THIS FILE FOR NEW CONFIG
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// Worked on by: Ashley Bickham and Siri Avula
+// TODO: FIX THIS FILE FOR NEW CONFIG
 // import { setupURLPolyfill } from "react-native-url-polyfill"
-// import axios from 'axios';
-// // import { APIKeys } from "../APIKeys";
+import axios from 'axios';
 
-// // setup syntax for URLs for API Call
+// setup syntax for URLs for API Call
 // setupURLPolyfill();
 
-// const TranslationService = {
-  
-//     // detects language and returns language code as a string
-//     async detectLanguage(text) : Promise<string> {
-//       if (text === undefined || text == ""){return ""}
-//       // to hold language value
-//       let lang;
+const TranslationService = {
 
-//       // setting up API call, text is text to be TLed
-//       const encodedParams = new URLSearchParams();
-//       encodedParams.set('text', text);
+    // detects language and returns language code as a string
+    async detectLanguage(text) : Promise<string> {
+      if (text === undefined || text == ""){return ""}
+      // to hold language value
+      let lang;
 
-//       const options = {
-//         method: 'POST',
-//         url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/detect-language',
-//         headers: {
-//           'content-type': 'application/x-www-form-urlencoded',
-//           'X-RapidAPI-Key': APIKeys.TLKey,
-//           'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
-//         },
-//         data: encodedParams,
-//       };
+      // setting up API call, text is text to be TLed
+      const encodedParams = new URLSearchParams();
+      encodedParams.set('text', text);
 
-//       // attempts API call and grabs language code detected
-//       try {
-//         const response = await axios.request(options);
-//         lang = response.data.source_lang_code;
-//       } catch (error) {
-//         console.error("error Detecting Lang: " + error);
-//       }
+      const options = {
+        method: 'POST',
+        url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/detect-language',
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'X-RapidAPI-Key': process.env.GTRANSLATION_API_KEY,
+          'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
+        },
+        data: encodedParams,
+      };
 
-//       return lang;
-//     },
+      // attempts API call and grabs language code detected
+      try {
+        const response = await axios.request(options);
+        lang = response.data.source_lang_code;
+      } catch (error) {
+        console.error("error Detecting Lang: " + error);
+      }
 
-//     /*translates <text> from language <from> (what lang <text> is initially in) into language <into>
-//     */
-//     async translateFRINTO(text, from, into) : Promise<string> {
-//       if ( text === undefined || text == "" || from === undefined || from == "" || into === undefined || into == ""){return ""}
+      return lang;
+    },
 
-//       // holds the translation
-//       let TLed;
+    /*translates <text> from language <from> (what lang <text> is initially in) into language <into>
+    */
+    async translateFRINTO(text, from, into) : Promise<string> {
+      if ( text === undefined || text == "" || from === undefined || from == "" || into === undefined || into == ""){return ""}
 
-//       // only runs translation call when <from> language has been provided and <from> != <into>
-//       if (from != into && from != '') {
-//         // setting up API call
-//         const encodedParams = new URLSearchParams();
-//         encodedParams.set('from', from );
-//         encodedParams.set('to', into );
-//         encodedParams.set('text', text);
-//         const options = {
-//         method: 'POST',
-//         url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
-//         headers: {
-//             'content-type': 'application/x-www-form-urlencoded',
-//             'X-RapidAPI-Key': APIKeys.TLKey,
-//             'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
-//         },
-//         data: encodedParams,
-//         };
+      // holds the translation
+      let TLed;
 
-//         // makes API call and parses translation from json response
-//         try {
-//             const response = await axios.request(options);
-//             TLed = response.data.trans;
-//         } catch (error : any) {
-//             console.error('ERROR TL FRINTO: ' +error);
-//             TLed = 'ERROR: ' + error.response.status;
-//         }
-//       }
-//       // when language you want to translate <from> and <into> are the same, do not need to translate the text
-//       else{
-//         TLed = text;
-//       }
+      // only runs translation call when <from> language has been provided and <from> != <into>
+      if (from != into && from != '') {
+        // setting up API call
+        const encodedParams = new URLSearchParams();
+        encodedParams.set('from', from );
+        encodedParams.set('to', into );
+        encodedParams.set('text', text);
+        const options = {
+        method: 'POST',
+        url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': process.env.GTRANSLATION_API_KEY,
+            'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
+        },
+        data: encodedParams,
+        };
 
-//       return TLed;
-//     },
+        // makes API call and parses translation from json response
+        try {
+            const response = await axios.request(options);
+            TLed = response.data.trans;
+        } catch (error : any) {
+            console.error('ERROR TL FRINTO: ' +error);
+            TLed = 'ERROR: ' + error.response.status;
+        }
+      }
+      // when language you want to translate <from> and <into> are the same, do not need to translate the text
+      else{
+        TLed = text;
+      }
 
-//     /*translates without being told from what language to translate from
-//     only used with large chunks of text like lyrics
-//     */
-//     async translateAuto(text, into) : Promise<string> {
-//       if ( text === undefined || text == "" || into === undefined || into == ""){return ""}
-//       // holds the translation
-//       let TLed;
+      return TLed;
+    },
 
-//       // setting up API call
-//       const encodedParams = new URLSearchParams();
-//       encodedParams.set('from', 'auto' );
-//       encodedParams.set('to', into);
-//       encodedParams.set('text', text);
-//       const options = {
-//       method: 'POST',
-//       url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
-//       headers: {
-//           'content-type': 'application/x-www-form-urlencoded',
-//           'X-RapidAPI-Key': APIKeys.TLKey,
-//           'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
-//       },
-//       data: encodedParams,
-//       };
+    /*translates without being told from what language to translate from
+    only used with large chunks of text like lyrics
+    */
+    async translateAuto(text, into) : Promise<string> {
+      if ( text === undefined || text == "" || into === undefined || into == ""){return ""}
+      // holds the translation
+      let TLed;
 
-//       // makes API call and parses translation from JSON response
-//       try {
-//         const response = await axios.request(options);
-//         TLed = response.data.trans;
-//       } catch (error: any) {
-//           console.error("ERROR from TLAuto: "+ error);
-//           TLed = 'ERROR: ' + error.response.status
-//       }
-//     return TLed;
-//   },
+      // setting up API call
+      const encodedParams = new URLSearchParams();
+      encodedParams.set('from', 'auto' );
+      encodedParams.set('to', into);
+      encodedParams.set('text', text);
+      const options = {
+      method: 'POST',
+      url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
+      headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'X-RapidAPI-Key': process.env.GTRANSLATION_API_KEY,
+          'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
+      },
+      data: encodedParams,
+      };
 
-// }
+      // makes API call and parses translation from JSON response
+      try {
+        const response = await axios.request(options);
+        TLed = response.data.trans;
+      } catch (error: any) {
+          console.error("ERROR from TLAuto: "+ error);
+          TLed = 'ERROR: ' + error.response.status
+      }
+    return TLed;
+  },
 
-// export default TranslationService;
+}
+
+export default TranslationService;
