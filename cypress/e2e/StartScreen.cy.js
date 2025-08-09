5560d936a36c2acef466fc7851d0d61520bcddb3
Vivian D'Souza <vivian.dsouza.789@gmail.com>
2025-08-09 16:11:19 -0400

    wrote login w/ existing user ITest

--- cypress/e2e/StartScreen.cy.js
+++ cypress/e2e/StartScreen.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://127.0.0.1:5173/')
-  })
-})
+describe("User login/Signup flow", () => {
+  it("should signup a new User", () => {
+    cy.visit("https://127.0.0.1:5173/");
+    cy.contains('[aria-label="title"]', "LYRAQUIST").should("be.visible");
+    cy.get("[aria-label='signupClick']").click();
+  });
+
+  it("should login an existing User", () => {
+    cy.visit("https://127.0.0.1:5173/");
+    cy.contains('[aria-label="title"]', "LYRAQUIST").should("be.visible");
+    cy.get("[aria-label='loginClick']").click();
+    cy.get("[aria-label='username']").click().type("viviand@email.sc.edu");
+    cy.get("[aria-label='password']").click().type("1234567");
+    cy.get("[data-testid='login-button']").click();
+    cy.wait(3000);
+  });
+});
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -265,6 +265,7 @@ const LoginScreen: React.FC = () => {
         type="submit"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         onClick={handleLyraquistLogIn}
+        data-testid="login-button"
       >
         <ToastContainer />
         Login
