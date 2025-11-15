34bcdc88210821d10c7cb062abcd31009baa149a
Vivian D'Souza <vivian.dsouza.789@gmail.com>
2025-11-14 21:59:06 -0500

    put a profanity filter on the search. returns mid-word results instead of profane ones

--- src/services/SearchSpotify.ts
+++ src/services/SearchSpotify.ts
@@ -3,47 +3,57 @@
 import axios from "axios";
 import { refresh, checkRefreshNeeded } from "../services/spotifyAuth";
 import TokenReaderWriter from "./firebase/TokenReaderWriter";
+import LeoProfanity from "leo-profanity";
 
 const SearchSpotify = {
   async searchForSong(title: string): Promise<any> {
-    console.log("SEARCHING>.." + title);
+    const filter = LeoProfanity;
+    const isProfanity = filter.check(title);
+
     if ((await checkRefreshNeeded(new Date())) == "true") {
       await TokenReaderWriter.getRefreshToken().then((refreshToken) => {
         refresh(refreshToken);
         localStorage.setItem("needs_refresh", "false");
       });
     }
-    // // Define function to get the song from Spotify API
-    const getSong = function () {
-      return new Promise<object[]>((resolve) => {
-        // Get user access code
-        TokenReaderWriter.getAccessToken().then((accessCode) => {
-          // Makes request to Spotify API for song search
-          axios({
-            url: "https://api.spotify.com/v1/search?q=" + title + "&type=track&limit=50", // Remove "&limit=1"
-            method: "GET",
-            headers: {
-              authorization: "Bearer " + accessCode,
-            },
-          })
-            .then(async (res) => {
-              const data = await res.data;
 
-              // const safeTracks = data.tracks.filter((track) => track["id"] == ""); 
-
-              // console.log("SAFE TRACKS", safeTracks);
-
-              // resolve(safeTracks.items || []); // Ensure 'items' is defined or provide an empty array
-              resolve(data.tracks.items || []); // Ensure 'items' is defined or provide an empty array
+    if (isProfanity === false) {
+      const getSong = function () {
+        return new Promise<object[]>((resolve) => {
+          // Get user access code
+          TokenReaderWriter.getAccessToken().then((accessCode) => {
+            // Makes request to Spotify API for song search
+            axios({
+              url:
+                "https://api.spotify.com/v1/search?q=" +
+                title +
+                "&type=track&limit=50", // Remove "&limit=1"
+              method: "GET",
+              headers: {
+                authorization: "Bearer " + accessCode,
+              },
             })
-            .catch((err) => {
-              return err;
-            });
-        });
-      });
-    };
+              .then(async (res) => {
+                const data = await res.data;
 
-    return getSong();
+                // const safeTracks = data.tracks.filter((track) => track["id"] == "");
+
+                // console.log("SAFE TRACKS", safeTracks);
+
+                // resolve(safeTracks.items || []); // Ensure 'items' is defined or provide an empty array
+                resolve(data.tracks.items || []); // Ensure 'items' is defined or provide an empty array
+              })
+              .catch((err) => {
+                return err;
+              });
+          });
+        });
+      };
+      return getSong();
+    } else {
+      console.log("Profanity detected in search term.");
+      return Promise.resolve([]);
+    }
   },
 
   //   // Get tracks from Spotify playlist by playlistID
